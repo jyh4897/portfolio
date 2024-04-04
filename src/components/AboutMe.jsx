46e7bc5500a1e4778bcbadb13c06d5e67f40ec3c
jyh4897 <jyh4897@gmail.com>
2024-04-04 11:39:42 +0900

    수정

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -35,7 +35,11 @@ export default function AboutMe() {
             <Col className="d-flex flex-column text-center">
               <Container>
                 <p>{bio}</p>
-                {moreInfo && <p>{moreInfo}</p>}
+                {/* {moreInfo && <p>{moreInfo}</p>} */}
+                <p>마크 주커버그와 페이스북덕에 꿈이 생긴 </p>
+                <p>동국대 법학과를 졸업한 주니어 개발자 전윤호입니다.</p>
+                <p>제 1회 KEG 서든어택 인천지역대표 경력 보유 중이며</p>
+                <p>게임과 개발에 몰두하여 그 속에서 가치를 이끌어내고자 합니다.</p>
               </Container>
             </Col>
             <Col className="d-none d-md-block text-center">
